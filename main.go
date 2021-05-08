bc71b42d5aecd57148e443b264298d2e74605ba2
AlastorTh <alexkasatkin90@gmail.com>
2021-05-08 13:19:33 +0300

    db connect set

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FirstName string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	LastName  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+var client *mongo.Client
+
+func main() {
+	fmt.Println("Starting the app...")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	router := mux.NewRouter()
+	http.ListenAndServe(":12345", router)
 }
