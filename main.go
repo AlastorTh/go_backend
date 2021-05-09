da9bd52081098b4bcb0f23187cee22b830a28485
AlastorTh <alexkasatkin90@gmail.com>
2021-05-09 13:37:56 +0300

    db connected

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/AlastorTh/go_backend/config"
+	"github.com/AlastorTh/go_backend/database"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	conf := config.GetConfig()
-	fmt.Println(conf)
+	db := database.ConnectDB(conf.Mongo)
+
+	fmt.Println(db)
+
+	router := mux.NewRouter()
+	http.ListenAndServe(":8080", router)
 }
